0f5c23986a39113f103a2a326c88a3495e83ea0a
Prathik Rajendran M <prathik.rajendran@go-jek.com>
2022-07-20 09:55:53 +0530

    Change UI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use std::{thread,time};
+use indicatif::{ProgressBar,ProgressStyle};
 
 fn main() {
-    let four_seconds = time::Duration::new(4, 0);
-    let six_seconds = time::Duration::new(6, 0);
+    let hundred_ms = time::Duration::from_millis(100);
+
     for i in 0..30 {
-        println!("{} Breath In", i);
-        thread::sleep(four_seconds);
-        println!("{} Breath Out", i);
-        thread::sleep(six_seconds);
+        let pb = ProgressBar::new(40).with_message("Breath In");
+        pb.set_style(ProgressStyle::default_bar()
+                     .template("{msg} {bar:40.cyan/blue} [{elapsed_precise}]")
+                     .progress_chars("##-"));
+
+        for _ in 0..40 {
+            thread::sleep(hundred_ms);
+            pb.inc(1);
+        }
+        pb.finish_with_message("Done");
+        let out = ProgressBar::new(60).with_message("Breath Out");
+        out.set_style(ProgressStyle::default_bar()
+                      .template("{msg} {bar:40.cyan/blue} [{elapsed_precise}]")
+                      .progress_chars("##-"));
+
+        for _ in 0..60 {
+            thread::sleep(hundred_ms);
+            out.inc(1);
+        }
+        out.finish_with_message(format!("Done {}", i + 1));
     }
 }
